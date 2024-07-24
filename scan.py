a10fc43c999a5791528b4063601605ee8330a5b1
Ian <ianloblaw@gmail.com>
2024-07-24 09:47:18 -0400

    Fix formatting using black

--- scan.py
+++ scan.py
@@ -41,17 +41,18 @@ from common import S3_ENDPOINT
 from common import create_dir
 from common import get_timestamp
 
+
 # https://github.com/python/cpython/blob/v3.11.2/Lib/distutils/util.py#L308
-def strtobool (val):
+def strtobool(val):
     """Convert a string representation of truth to true (1) or false (0).
     True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
     'val' is anything else.
     """
     val = val.lower()
-    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+    if val in ("y", "yes", "t", "true", "on", "1"):
         return 1
-    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+    elif val in ("n", "no", "f", "false", "off", "0"):
         return 0
     else:
         raise ValueError("invalid truth value %r" % (val,))
